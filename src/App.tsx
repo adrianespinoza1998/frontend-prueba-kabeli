47c1b6c228f5902fa852edfd13f8573cc95cb2a6
Adrian Espinoza <adrianespinozaarevalo@gmail.com>
2023-04-30 13:36:26 -0400

    fix app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { Indicador } from "./components/Indicador";
 import { IndicadorScreen } from "./components/IndicadorScreen";
 import { useFetch } from "./hooks/useFetch";
 import "./index.css";
-import { changeDateFormat } from "./utils/dates";
 
 const App = () => {
   const { data, error, isLoading } = useFetch(
@@ -16,25 +14,23 @@ const App = () => {
       {isLoading && <div>Loading...</div>}
       {error && <div>{error}</div>}
       {data && (
-        <div>
-          <IndicadorScreen
-            version={data.version}
-            autor={data.autor}
-            fecha={data.fecha}
-            uf={data.uf}
-            ivp={data.ivp}
-            dolar={data.dolar}
-            dolar_intercambio={data.dolar_intercambio}
-            euro={data.euro}
-            ipc={data.ipc}
-            utm={data.utm}
-            imacec={data.imacec}
-            tpm={data.tpm}
-            libra_cobre={data.libra_cobre}
-            tasa_desempleo={data.tasa_desempleo}
-            bitcoin={data.bitcoin}
-          />
-        </div>
+        <IndicadorScreen
+          version={data.version}
+          autor={data.autor}
+          fecha={data.fecha}
+          uf={data.uf}
+          ivp={data.ivp}
+          dolar={data.dolar}
+          dolar_intercambio={data.dolar_intercambio}
+          euro={data.euro}
+          ipc={data.ipc}
+          utm={data.utm}
+          imacec={data.imacec}
+          tpm={data.tpm}
+          libra_cobre={data.libra_cobre}
+          tasa_desempleo={data.tasa_desempleo}
+          bitcoin={data.bitcoin}
+        />
       )}
     </div>
   );
